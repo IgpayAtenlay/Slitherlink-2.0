0c8acdc445615badd05debb28c11cad47269c006
IgpayAtenlay <38770334+IgpayAtenlay@users.noreply.github.com>
2026-01-01 22:38:32 -0500

    clean up code

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,7 @@
-import Enums.Difficulty;
-import PuzzleLoading.ExtractDataToVariable;
-import PuzzleLoading.ParseData;
-import PuzzleLoading.TextData;
 import Visuals.Frame;
-
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         new Frame();
-//        solvePuzzle();
-    }
-
-    public static void solvePuzzle() {
-        ArrayList<ArrayList<TextData>> puzzles = ParseData.splitLists(ExtractDataToVariable.extract(Difficulty.TOUGH, 20, 1, 1));
-//        MemorySet memory = new MemorySet(ParseData.parsePuzzle(puzzles.get(2)));
-//        memory.autoSolve(false);
-//        memory.linesCalculationToVisible();
-//        new Frame(memory);
     }
 }
