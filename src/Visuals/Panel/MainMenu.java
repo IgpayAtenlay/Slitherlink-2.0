5e851a01e769502e75f5db35a8ea7df9d316db56
IgpayAtenlay <38770334+IgpayAtenlay@users.noreply.github.com>
2026-01-06 11:22:57 -0500

    new puzzles are custom sized now

--- src/Visuals/Panel/MainMenu.java
+++ src/Visuals/Panel/MainMenu.java
@@ -24,8 +24,8 @@ public class MainMenu extends JPanel {
 
         createText("Main Menu");
 
-        createButton("New 20x20 Puzzle", e -> mainMenuInteractions.newPuzzle(20, 20));
-        createButton("Generate Puzzle", e -> mainMenuInteractions.generatePuzzle(20, 20));
+        puzzleCreatorButton("New Puzzle", (x, y) -> mainMenuInteractions.newPuzzle(x, y));
+        puzzleCreatorButton("Generate Puzzle", (x, y) -> mainMenuInteractions.generatePuzzle(x, y));
         createButton("Load", e -> mainMenuInteractions.load());
         createButton("Import PDFs", e -> mainMenuInteractions.importPDFs());
     }
@@ -39,12 +39,55 @@ public class MainMenu extends JPanel {
     }
 
     public void createButton(String text, ActionListener l) {
-        JButton checkAccuracy = new JButton(text);
+        JButton button = new JButton(text);
         int buttonHeight = getFont().getSize() + 2;
         int buttonWidth = getFontMetrics(getFont()).stringWidth(text) + 50;
-        checkAccuracy.setBounds(componentX, componentY, buttonWidth, buttonHeight);
-        checkAccuracy.addActionListener(l);
-        add(checkAccuracy);
+        button.setBounds(componentX, componentY, buttonWidth, buttonHeight);
+        button.addActionListener(l);
+        add(button);
+        componentY += buttonHeight + BUTTON_GAP;
+    }
+    @FunctionalInterface
+    interface PuzzleCreator {
+        void create(int x, int y);
+    }
+    public void puzzleCreatorButton(String text, PuzzleCreator puzzleCreator) {
+        JTextField field1 = new JTextField(10);
+        JTextField field2 = new JTextField(10);
+        JLabel label = new JLabel("x");
+        JButton button = new JButton(text);
+
+        int fieldWidth = getFontMetrics(getFont()).stringWidth("20") + 5;
+        int labelWidth = getFontMetrics(getFont()).stringWidth("x");
+        int buttonWidth = getFontMetrics(getFont()).stringWidth(text) + 50;
+        int buttonHeight = getFont().getSize() + 2;
+
+        int currentX = componentX;
+
+        field1.setBounds(currentX, componentY, fieldWidth, buttonHeight);
+        currentX += fieldWidth + BUTTON_GAP;
+        label.setBounds(currentX, componentY, labelWidth, buttonHeight);
+        currentX += labelWidth + BUTTON_GAP;
+        field2.setBounds(currentX, componentY, fieldWidth, buttonHeight);
+        currentX += fieldWidth + BUTTON_GAP;
+        button.setBounds(currentX, componentY, buttonWidth, buttonHeight);
+
+        button.addActionListener(e -> {
+            try {
+                int text1 = Integer.parseInt(field1.getText());
+                int text2 = Integer.parseInt(field2.getText());
+                if (text1 > 0 && text1 < 100 && text2 > 0 && text2 < 100) {
+                    puzzleCreator.create(text1, text2);
+                }
+            } catch (Exception ignored) {
+
+            }
+        });
+
+        add(field1);
+        add(label);
+        add(field2);
+        add(button);
         componentY += buttonHeight + BUTTON_GAP;
     }
 }
